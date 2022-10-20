f0e132a9381608602b367cc8c2ad6c277986d7fc
vivalkm <vivalkm@gmail.com>
2022-10-19 21:55:32 -0700

    Complete navbar and log fragment

---
+++ app/src/main/java/com/vivalkm/bitfit/LogFragment.kt
@@ -0,0 +1,186 @@
+package com.vivalkm.bitfit
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class LogFragment(private val itemDao: ItemDao) : Fragment(), RecyclerViewInterface {
+    lateinit var wlItems: MutableList<Item>;
+    lateinit var wlStatusImages: MutableList<Int>
+    lateinit var adapter: ItemAdapter
+    lateinit var viewLog: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        wlStatusImages = ArrayList(listOf(R.drawable.ic_baseline_favorite_border_24, R.drawable.ic_baseline_favorite_24))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Change this statement to store the view in a variable instead of a return statement
+        viewLog = inflater.inflate(R.layout.fragment_log, container, false)
+        return viewLog
+    }
+
+    companion object {
+        fun newInstance(itemDao: ItemDao): LogFragment {
+            return LogFragment(itemDao)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val editTextName = view.findViewById<EditText>(R.id.editTextName)
+        val editTextNumber = view.findViewById<EditText>(R.id.editTextNumber)
+        val editTextNote = view.findViewById<EditText>(R.id.editTextNote)
+        val submitBtn = view.findViewById<Button>(R.id.submitBtn)
+        val itemsRv = view.findViewById<RecyclerView>(R.id.itemsRv)
+        wlItems = ArrayList<Item>()
+        // Create adapter passing in the list of items
+        adapter = ItemAdapter(wlItems, wlStatusImages, this)
+        // Attach the adapter to the RecyclerView to populate items
+        itemsRv.adapter = adapter
+        // Set layout manager to position the items
+        itemsRv.layoutManager = LinearLayoutManager(view.context)
+
+        reFresh()
+
+        // swipe left to remove item
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                // this method is called
+                // when the item is moved.
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                // this method is called when we swipe our item to right direction.
+                val position = viewHolder.adapterPosition
+
+                // delete item from database
+                lifecycleScope.launch(Dispatchers.IO) {
+                    itemDao.delete(
+                        wlItems[position].id
+                    )
+                }
+                // reload recycler view
+                reFresh()
+            }
+        }).attachToRecyclerView(itemsRv)
+
+//         click button to add item
+        submitBtn.setOnClickListener {
+            // get data from textEditViews, and create new item
+            val name = editTextName.text.toString()
+            val number = try {
+                editTextNumber.text.toString().toFloat()
+            } catch (e: NumberFormatException) {
+                0.0F
+            }
+            val notes = editTextNote.text.toString()
+
+            // We need to update any ItemDao interaction to run on another thread
+            // Since we are using Room and writing to files, we want to use the IO Dispatcher
+            lifecycleScope.launch(Dispatchers.IO) {
+                itemDao.insert(
+                    ItemEntity(
+                        name = name,
+                        amount = number,
+                        notes = notes,
+                        isLiked = 0
+                    )
+                )
+            }
+            reFresh()
+
+            // clean up input areas
+            editTextName.text.clear()
+            editTextNumber.text.clear()
+            editTextNote.text.clear()
+        }
+    }
+
+    // reload data from db and refresh recycler view list
+    fun reFresh() {
+        lifecycleScope.launch {
+            itemDao.getAll().collect { databaseList ->
+                databaseList.map { entity ->
+                    Item(
+                        entity.id,
+                        entity.name,
+                        entity.amount,
+                        entity.notes,
+                        entity.isLiked
+                    )
+                }.also { mappedList ->
+                    wlItems.clear()
+                    wlItems.addAll(mappedList)
+                    adapter.notifyDataSetChanged()
+                }
+            }
+        }
+    }
+
+    // click to toggle check status
+    override fun onItemClick(position: Int) {
+        val item = wlItems[position]
+        if (item.isLiked == 1) {
+            item.isLiked = 0
+        } else {
+            item.isLiked = 1
+        }
+        lifecycleScope.launch(Dispatchers.IO) {
+            itemDao.update(
+                item.id, item.isLiked
+            )
+        }
+        reFresh()
+    }
+
+    // long click to open url in notes, if not valid link then search for the name of item on google
+    override fun onItemLongClick(position: Int) {
+        val item = wlItems[position]
+        if (item.note.isNotEmpty()) {
+            try {
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(item.note))
+                ContextCompat.startActivity(viewLog.context, browserIntent, null)
+            } catch (e: ActivityNotFoundException) {
+                val browserIntent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://www.google.com/search?q=" + item.name)
+                )
+                ContextCompat.startActivity(viewLog.context, browserIntent, null)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(
+                    viewLog.context,
+                    "Not able to look up the item on internet.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+
+}
--- app/src/main/java/com/vivalkm/bitfit/MainActivity.kt
+++ app/src/main/java/com/vivalkm/bitfit/MainActivity.kt
@@ -1,24 +1,11 @@
 package com.vivalkm.bitfit
 
-import android.content.ActivityNotFoundException
-import android.content.Intent
-import android.graphics.drawable.AnimationDrawable
-import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.launch
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(), RecyclerViewInterface {
+class MainActivity : AppCompatActivity() {
     lateinit var wlItems: MutableList<Item>;
     lateinit var wlStatusImages: MutableList<Int>
     lateinit var adapter: ItemAdapter
@@ -27,146 +14,29 @@ class MainActivity : AppCompatActivity(), RecyclerViewInterface {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // animated background
-        val constraintLayout = findViewById<ConstraintLayout>(R.id.mainLayout)
-        val animationDrawable = constraintLayout.background as AnimationDrawable
-        animationDrawable.setEnterFadeDuration(2500)
-        animationDrawable.setExitFadeDuration(2500)
-        animationDrawable.start()
+        val itemDao = (application as BitFitApplication).db.itemDao()
+        val navbar = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val logFragment = LogFragment(itemDao)
+        val dashboardFragment = LogFragment(itemDao)
 
-        wlStatusImages = ArrayList(listOf(R.drawable.ic_baseline_favorite_border_24, R.drawable.ic_baseline_favorite_24))
-
-        val editTextName = findViewById<EditText>(R.id.editTextName)
-        val editTextNumber = findViewById<EditText>(R.id.editTextNumber)
-        val editTextNote = findViewById<EditText>(R.id.editTextNote)
-        val submitBtn = findViewById<Button>(R.id.submitBtn)
-        val itemsRv = findViewById<RecyclerView>(R.id.itemsRv)
-        wlItems = ArrayList<Item>()
-        // Create adapter passing in the list of items
-        adapter = ItemAdapter(wlItems, wlStatusImages, this)
-        // Attach the adapter to the RecyclerView to populate items
-        itemsRv.adapter = adapter
-        // Set layout manager to position the items
-        itemsRv.layoutManager = LinearLayoutManager(this)
-
-        reFresh()
-
-        // swipe left to remove item
-        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                // this method is called
-                // when the item is moved.
-                return false
+        navbar.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            // Call helper method to swap the FrameLayout with the fragment
+            when (item.itemId) {
+                R.id.action_log -> replaceFragment(logFragment)
+                R.id.action_dashboard -> replaceFragment(logFragment)
             }
-
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                // this method is called when we swipe our item to right direction.
-                val position = viewHolder.adapterPosition
-
-                // delete item from database
-                lifecycleScope.launch(IO) {
-                    (application as BitFitApplication).db.itemDao().delete(
-                        wlItems[position].id
-                    )
-                }
-                // reload recycler view
-                reFresh()
-            }
-        }).attachToRecyclerView(itemsRv)
-
-        // click button to add item
-        submitBtn.setOnClickListener {
-            // get data from textEditViews, and create new item
-            val name = editTextName.text.toString()
-            val number = try {
-                editTextNumber.text.toString().toFloat()
-            } catch (e: NumberFormatException) {
-                0.0F
-            }
-            val notes = editTextNote.text.toString()
-
-            // We need to update any ItemDao interaction to run on another thread
-            // Since we are using Room and writing to files, we want to use the IO Dispatcher
-            lifecycleScope.launch(IO) {
-                (application as BitFitApplication).db.itemDao().insert(
-                    ItemEntity(
-                        name = name,
-                        amount = number,
-                        notes = notes,
-                        isLiked = 0
-                    )
-                )
-            }
-            reFresh()
-
-            // clean up input areas
-            editTextName.text.clear()
-            editTextNumber.text.clear()
-            editTextNote.text.clear()
+            true
         }
+
+        // Set default selection
+        navbar.selectedItemId = R.id.action_log
     }
 
-    // click to toggle check status
-    override fun onItemClick(position: Int) {
-        val item = wlItems[position]
-        if (item.isLiked == 1) {
-            item.isLiked = 0
-        } else {
-            item.isLiked = 1
-        }
-        lifecycleScope.launch(IO) {
-            (application as BitFitApplication).db.itemDao().update(
-                item.id, item.isLiked
-            )
-        }
-        reFresh()
-    }
-
-    // long click to open url in notes, if not valid link then search for the name of item on google
-    override fun onItemLongClick(position: Int) {
-        val item = wlItems[position]
-        if (item.note.isNotEmpty()) {
-            try {
-                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(item.note))
-                ContextCompat.startActivity(this, browserIntent, null)
-            } catch (e: ActivityNotFoundException) {
-                val browserIntent = Intent(
-                    Intent.ACTION_VIEW,
-                    Uri.parse("https://www.google.com/search?q=" + item.name)
-                )
-                ContextCompat.startActivity(this, browserIntent, null)
-            } catch (e: ActivityNotFoundException) {
-                Toast.makeText(
-                    this,
-                    "Not able to look up the item on internet.",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-    }
-
-    // reload data from db and refresh recycler view list
-    fun reFresh() {
-        lifecycleScope.launch {
-            (application as BitFitApplication).db.itemDao().getAll().collect { databaseList ->
-                databaseList.map { entity ->
-                    Item(
-                        entity.id,
-                        entity.name,
-                        entity.amount,
-                        entity.notes,
-                        entity.isLiked
-                    )
-                }.also { mappedList ->
-                    wlItems.clear()
-                    wlItems.addAll(mappedList)
-                    adapter.notifyDataSetChanged()
-                }
-            }
-        }
+    private fun replaceFragment(logFragment: LogFragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_frame_layout, logFragment)
+        fragmentTransaction.commit()
     }
 }
